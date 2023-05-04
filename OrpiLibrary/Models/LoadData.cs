11a0fd88fe736580de538f0124f19bf78564cda6
Gleb Evlakhov <42343481+gleb321@users.noreply.github.com>
2023-05-04 03:36:14 +0300

    added main metrics to monitoring

--- OrpiLibrary/Models/LoadData.cs
+++ OrpiLibrary/Models/LoadData.cs
@@ -6,5 +6,6 @@ public class LoadData {
     [JsonPropertyName("cpu_usage")] public double CpuUsage { get; set; }
     [JsonPropertyName("memory_usage")] public double MemoryUsage { get; set; }
     [JsonPropertyName("disk_usage")] public double DiskUsage { get; set; }
-    [JsonPropertyName("network_usage")] public double NetworkUsage { get; set; }
+    [JsonPropertyName("total_input_network_usage")] public double TotalInputNetworkUsage { get; set; }
+    [JsonPropertyName("total_output_network_usage")] public double TotalOutputNetworkUsage { get; set; }
 }
